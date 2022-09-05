80239b2675acfd4a00ca11c14c010def6cce0657
mahmoud98-30 <psmahmoud@gmail.com>
2022-09-05 20:50:49 +0200

    attendance from lecturer

--- attendance/views.py
+++ attendance/views.py
@@ -94,10 +94,10 @@ def predict(face_aligned, svc, threshold=0.7):
 
     prob = svc.predict_proba(faces_encodings)
     result = np.where(prob[0] == np.amax(prob[0]))
-    if (prob[0][result[0]] <= threshold):
-        return ([-1], prob[0][result[0]])
+    if prob[0][result[0]] <= threshold:
+        return [-1], prob[0][result[0]]
 
-    return (result[0], prob[0][result[0]])
+    return result[0], prob[0][result[0]]
 
 
 def update_attendance(request, present, id):
@@ -245,15 +245,108 @@ def edit_attendance(request):
 
 
 def edit_attendance_in(request, course, student):
-    q_course = Course.objects.get(id=course)
-    q_student = User.objects.get(id=student)
-    q_attendance = Attendance.objects.get(student=q_student, course=q_course)
-    q_attendance.is_present = True
-    q_attendance.save(update_fields=['is_present'])
-    msg = (
-        'done')
-    messages.add_message(request, messages.SUCCESS, msg)
+    detector = dlib.get_frontal_face_detector()
+
+    predictor = dlib.shape_predictor(
+        'account/face_recognition_data/shape_predictor_68_face_landmarks.dat')  # Add path to the shape predictor ######CHANGE TO RELATIVE PATH LATER
+    svc_save_path = "account/face_recognition_data/svc.sav"
+
+    with open(svc_save_path, 'rb') as f:
+        svc = pickle.load(f)
+    fa = FaceAligner(predictor, desiredFaceWidth=96)
+    encoder = LabelEncoder()
+    encoder.classes_ = np.load('account/face_recognition_data/classes.npy')
+
+    faces_encodings = np.zeros((1, 128))
+    no_of_faces = len(svc.predict_proba(faces_encodings)[0])
+    count = dict()
+    present = dict()
+    log_time = dict()
+    start = dict()
+    for i in range(no_of_faces):
+        count[encoder.inverse_transform([i])[0]] = 0
+        present[encoder.inverse_transform([i])[0]] = False
+
+    vs = VideoStream(src=0).start()
+
+    sampleNum = 0
+
+    while (True):
+
+        frame = vs.read()
+
+        frame = imutils.resize(frame, width=800)
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = detector(gray_frame, 0)
+
+        for face in faces:
+            print("INFO : inside for loop")
+            (x, y, w, h) = face_utils.rect_to_bb(face)
+
+            face_aligned = fa.align(frame, gray_frame, face)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+            (pred, prob) = predict(face_aligned, svc)
+
+            if (pred != [-1]):
+
+                person_name = encoder.inverse_transform(np.ravel([pred]))[0]
+                pred = person_name
+                if count[pred] == 0:
+                    start[pred] = time.time()
+                    count[pred] = count.get(pred, 0) + 1
+
+                if count[pred] == 4 and (time.time() - start[pred]) > 1.2:
+                    count[pred] = 0
+                else:
+                    # if count[pred] == 4 and (time.time()-start) <= 1.5:
+                    present[pred] = True
+                    log_time[pred] = datetime.datetime.now()
+                    count[pred] = count.get(pred, 0) + 1
+                    print(pred, present[pred], count[pred])
+                cv2.putText(frame, str(person_name) + str(prob), (x + 6, y + h - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                            (0, 255, 0), 1)
+
+            else:
+                person_name = "unknown"
+                cv2.putText(frame, str(person_name), (x + 6, y + h - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+        # cv2.putText()
+        # Before continuing to the next loop, I want to give it a little pause
+        # waitKey of 100 millisecond
+        # cv2.waitKey(50)
+
+        # Showing the image in another window
+        # Creates a window with window name "Face" and with the image img
+        cv2.imshow("Mark Attendance - In - Press q to exit", frame)
+        # Before closing it we need to give a wait command, otherwise the open cv wont work
+        # @params with the millisecond of delay 1
+        # cv2.waitKey(1)
+        # To get out of the loop
+        key = cv2.waitKey(50) & 0xFF
+        if (key == ord("q")):
+            break
+
+    # Stoping the videostream
+    vs.stop()
+
+    # destroying all the windows
+    cv2.destroyAllWindows()
+    print(course)
+    update_attendance(request, present, course)
+    print(present)
     return redirect('/edit-attendance/')
+    # q_course = Course.objects.get(id=course)
+    # q_student = User.objects.get(id=student)
+    # q_attendance = Attendance.objects.get(student=q_student, course=q_course)
+    # q_attendance.is_present = True
+    # q_attendance.save(update_fields=['is_present'])
+    # msg = (
+    #     'done')
+    # messages.add_message(request, messages.SUCCESS, msg)
+    # return redirect('/edit-attendance/')
 
 
 def edit_attendance_out(request, course, student):
